456c881db6cfea725da3d1b85e246dd60b87c663
Katrina Pabuna <kinapabuna@gmail.com>
2023-05-22 21:56:38 +0800

    edit: remove __init__; funcs def to self; add return

--- calculator.py
+++ calculator.py
@@ -2,106 +2,105 @@
 
 # create class Calculator
 class Calculator:
-    # constructor
-    def __init__(self, add, sub, mul, div):
-        # intance variables
-        self.add = add
-        self.sub = sub 
-        self.mul = mul
-        self.div = div
 
-# def ask_operation
-def ask_operation():
-    '''Create def ask_operation where it lets the user to pick among
-    addition, subtraction, multiplication, and division.'''
-    user_input = input("\nWhat operation do you want to use?\n" +
-                        "a - Add\n" +
-                        "s - Subtract\n" +
-                        "m - Multiply\n" +
-                        "d - Divide\n\n")
-    if user_input.lower() == "a":
-        add() # should lead to add function
-    elif user_input.lower() == "s":
-        sub() # should lead to sub function
-    elif user_input.lower() == "m":
-        mul() # should lead to mul function
-    elif user_input.lower() == "d":
-        div() # should lead to div function
-    else:
-        print("Error. Invalid input.")
+    # def ask_operation
+    def ask_operation(self):
+        '''Create def ask_operation where it lets the user to pick among
+        addition, subtraction, multiplication, and division.'''
+        user_input = input("\nWhat operation do you want to use?\n" +
+                            "a - Add\n" +
+                            "s - Subtract\n" +
+                            "m - Multiply\n" +
+                            "d - Divide\n\n")
+        if user_input.lower() == "a":
+            Calculator.add(self) # should lead to add function
+        elif user_input.lower() == "s":
+            Calculator.sub(self) # should lead to sub function
+        elif user_input.lower() == "m":
+            Calculator.mul(self) # should lead to mul function
+        elif user_input.lower() == "d":
+            Calculator.div(self) # should lead to div function
+        else:
+            print("Error. Invalid input.")
+            exit()
+        return
 
-# def add
-def add():
-    # ask for inputs
-    try:
-        num1 = float(input("\nInput the first number: "))
-        num2 = float(input("Input the second number: "))
-    except ValueError:
-        print("Syntax Error: Invalid input")
-    # get the sum of the two inputs
-    sum = num1 + num2
-    print(str(num1), "+", str(num2) + 
-        "\nSum: " + str(sum))
-    retry()
+    # def add
+    def add(self):
+        # ask for inputs
+        try:
+            num1 = float(input("\nInput the first number: "))
+            num2 = float(input("Input the second number: "))
+        except ValueError:
+            print("Syntax Error: Invalid input")
+        # get the sum of the two inputs
+        sum = num1 + num2
+        print(str(num1), "+", str(num2) + 
+            "\nSum: " + str(sum))
+        Calculator.retry(self)
+        return
 
-# def subtract
-def sub():
-    # ask for inputs
-    try:
-        num1 = float(input("\nInput the first number: "))
-        num2 = float(input("Input the second number: "))
-    except ValueError:
-        print("Syntax Error: Invalid input")
-    # get the difference of the two inputs
-    diff = num1 - num2
-    print(str(num1), "-", str(num2) + 
-        "\nDifference: " + str(diff))
-    retry()
+    # def subtract
+    def sub(self):
+        # ask for inputs
+        try:
+            num1 = float(input("\nInput the first number: "))
+            num2 = float(input("Input the second number: "))
+        except ValueError:
+            print("Syntax Error: Invalid input")
+        # get the difference of the two inputs
+        diff = num1 - num2
+        print(str(num1), "-", str(num2) + 
+            "\nDifference: " + str(diff))
+        Calculator.retry(self)
+        return
 
-# def multiply
-def mul():
-    # ask for inputs
-    try:
-        num1 = float(input("\nInput the first number: "))
-        num2 = float(input("Input the second number: "))
-    except ValueError:
-        print("Syntax Error: Invalid input")
-    # get the product of the two inputs
-    prod = num1 * num2
-    print(str(num1), "*", str(num2) + 
-        "\nProduct: " + str(prod))
-    retry()
+    # def multiply
+    def mul(self):
+        # ask for inputs
+        try:
+            num1 = float(input("\nInput the first number: "))
+            num2 = float(input("Input the second number: "))
+        except ValueError:
+            print("Syntax Error: Invalid input")
+        # get the product of the two inputs
+        prod = num1 * num2
+        print(str(num1), "*", str(num2) + 
+            "\nProduct: " + str(prod))
+        Calculator.retry(self)
+        return
 
-# def division
-def div():
-    # ask for inputs
-    try:
-        num1 = float(input("\nInput the first number: "))
-        num2 = float(input("Input the second number: "))
-    except ValueError:
-        print("Syntax Error: Invalid input")
-    except ZeroDivisionError:
-        print("Math Error: Division by zero")
-    # get the quotient of the two inputs
-    quot = num1 / num2
-    print(str(num1), "/", str(num2) + 
-        "\nQuotient: " + str(quot))
-    retry()
+    # def division
+    def div(self):
+        # ask for inputs
+        try:
+            num1 = float(input("\nInput the first number: "))
+            num2 = float(input("Input the second number: "))
+        except ValueError:
+            print("Syntax Error: Invalid input")
+        except ZeroDivisionError:
+            print("Math Error: Division by zero")
+        # get the quotient of the two inputs
+        quot = num1 / num2
+        print(str(num1), "/", str(num2) + 
+            "\nQuotient: " + str(quot))
+        Calculator.retry(self)
+        return
 
-# def retry
-def retry():
-    '''Create def retry where it asks the user if they would
-    like to try again or not.''' 
-    user_input = input("\nWould you like to try again?\n" +
-                    "y - Yes\n" +
-                    "n - No\n\n")
-    if user_input.lower() == "y":
-        ask_operation()
-    elif user_input.lower() == "n":
-        print("Thank you!")
-        exit()
-    else:
-        print("Error. Invalid input.")
-    retry()
-
-ask_operation()
+    # def retry
+    def retry(self):
+        '''Create def retry where it asks the user if they would
+        like to try again or not.''' 
+        user_input = input("\nWould you like to try again?\n" +
+                        "y - Yes\n" +
+                        "n - No\n\n")
+        if user_input.lower() == "y":
+            Calculator.ask_operation(self)
+        elif user_input.lower() == "n":
+            print("Thank you!")
+            exit()
+        else:
+            print("Error. Invalid input.")
+            exit()
+        Calculator.retry(self)
+        return
--- calculator_call.py
+++ calculator_call.py
@@ -3,6 +3,23 @@
 # import class
 from calculator import Calculator
 
+calc = Calculator()
+
 # ==== start ====
-ask_operation()
-retry()
+# ask operation
+calc.ask_operation()
+
+# if the user enters "a"
+calc.add()
+
+# if the user enters "s"
+calc.sub()
+
+# if the user enters "m"
+calc.mul()
+
+# if the user enters "d"
+calc.div()
+
+# propts the user if they would like to try again
+calc.retry()
