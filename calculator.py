0a899c9b078b8ec0c9abb34656743afc3da79da4
Katrina Pabuna <kinapabuna@gmail.com>
2023-05-22 20:14:29 +0800

    def add

--- calculator.py
+++ calculator.py
@@ -2,23 +2,24 @@
 
 # create class Calculator
 class Calculator:
-
-# ask for inputs
-    def first_num(self):
-        try:
-            first_num = float(input("Input the first number: "))
-        except ValueError:
-            print("Input a number only.")
-            return first_num
-
-    def sec_num(self):
-        try:
-            sec_num = float(input("Input the second number: "))
-        except ValueError:
-            print("Input a number only.")
-            return sec_num
+    def __init__(self, add, sub, mul, div):
+        self.add = add
+        self.sub = sub 
+        self.mul = mul
+        self.div = div
 
 # def add
+def add():
+        # ask for inputs
+    try:
+        num1 = float(input("\nInput the first number: "))
+        num2 = float(input("Input the second number: "))
+    except ValueError:
+        print("Input a number only.")
+    # get the sum of the two inputs
+    sum = num1 + num2
+    print(str(num1), "+", str(num2) + 
+        "\nSum: " + str(sum))
 
 # def subtract
 
@@ -27,5 +28,17 @@ class Calculator:
 # def division
 
 # def ask_operation
-
+def ask_operation():
+    '''Create def ask_operation where it lets the user pick among
+    addition, subtraction, multiplication, and division.'''
+    user_input = input("\nWhat operation do you want to use?\n" +
+                        "a - Add\n" +
+                        "s - Subtract\n" +
+                        "m - Multiply\n" +
+                        "d - Divide\n\n")
+    if user_input.lower() == "a":
+        add()
+    ask_operation()
+                            
+ask_operation()
 # def retry
--- calculator_call.py
+++ calculator_call.py
@@ -3,8 +3,5 @@
 # import class
 from calculator import Calculator
 
-calc = Calculator()
-
 # ==== start ====
-num1 = calc.first_num()
-num2 = calc.sec_num()
+ask_operation()
