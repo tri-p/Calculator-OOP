f87db8f4cc3f1fd33646963f4cc48f665c250124
Katrina Pabuna <kinapabuna@gmail.com>
2023-05-22 18:00:18 +0800

    create class Calculator

---
+++ calculator.py
@@ -0,0 +1,14 @@
+# PABUNA KATRINA B. 
+
+# create class Calculator
+class Calculator:
+
+# def add
+
+# def subtract
+
+# def multiply
+
+# def division
+
+# def ask_operation
---
+++ calculator_call.py
@@ -0,0 +1,5 @@
+# PABUNA, KATRINA B. 
+
+# import class
+
+# ==== start ====
