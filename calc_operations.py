dd8b627f7c280049beea2c8cff932a66276bfef7
Katrina Pabuna <kinapabuna@gmail.com>
2023-06-03 19:46:16 +0800

    get sum using child class

--- calc_operations.py
+++ calc_operations.py
@@ -8,4 +8,11 @@ ui = UserInterface()
 calc = Calculator()
 
 class CalculatorOperations(UserInterface, Calculator):
-    pass
+    def add_ans(self):
+        print("\n")
+        num1 = ui.input_user()
+        num2 = ui.input_user()
+        sum = calc.add(num1, num2)
+        ans = print("\n\033[92m" + str(num1) + " + " + str(num2)) 
+        ui.print_sum(sum)
+        return 
--- calculator_program.py
+++ calculator_program.py
@@ -12,39 +12,18 @@ op = CalculatorOperations()
 # ==== start ====
 while True:
     # ask operation
-    user_input = ui.ask_operation()
+    user_input = ui.ask_operation().lower()
     if user_input == "a":
-        add_ans # should lead to add function
+        op.add_ans() # should lead to add function
     elif user_input == "s":
-        calc.sub() # should lead to sub function
+         # should lead to sub function
     elif user_input == "m": 
-        calc.mul() # should lead to mul function
+         # should lead to mul function
     elif user_input == "d":
-        calc.div() # should lead to div function
+         # should lead to div function
     else:
         print("\n\033[91mError. Invalid input.\033[97m\n")
         exit()
-
-    # ask to input two numbers
-    num1 = ui.input_user()
-    num2 = ui.input_user()
-
-    # if the user enters "a" - add
-    def add_ans(self):
-        sum = calc.add(num1, num2)
-        ui.print_sum(sum)
-
-    # if the user enters "s" - subtract
-    diff = calc.sub(num1, num2)
-    ui.print_diff(diff)
-
-    # if the user enters "m" - multiply
-    prod = calc.mul(num1, num2)
-    ui.print_prod(prod)
-
-    # if the user enters "d" - divide
-    quot = calc.div(num1, num2)
-    ui.print_quot(quot)
 
     # retry
     if not ui.retry():
